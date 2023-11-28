9c04e40673a1a631f4580e85d17539d4f4fe57e7
Пользователь <shohjaxondehqonboyev59@gmail.com>
2023-11-28 16:45:03 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+a =int(input("raqam: "))
+print(" javobi: " ,a**4 )
+
+
+# Remainder      2-misol
+a = int(input("Enter the divisor: " ))
+b = int(input("Enter the divident" ))
+c = a % b
+print("Result: ", c)
+
+side_length = int(input("uzunlikni toping: "))
+
+yuzi = uzunligi * uzunligi
+print("Result of area: " , yuzi)
+
+# Calculating the perimeter of the square
+perimeter = 4 * side_length
+print("Result of perimeter: " , perimeter)
+
+
+
+
+
+
+
+
